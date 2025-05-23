374af43e069ea181cd5834d6b2769c81d83a3756
Lahiru Vimukthi <lahiruvimukthi292@gmail.com>
2025-05-23 08:28:27 +0530

    feat: Add footer and copyright components, implement navigation and sidebar for user management
    - Created a new Copyright component for footer usage.
    - Developed a Footer component with company information, useful links, and social media icons.
    - Implemented a LogInNavbar for navigation in the login section.
    - Added a NavBar component for authenticated user navigation with logout functionality.
    - Created a SideBar component for admin user management with dynamic item rendering based on user role.
    - Established basic styling in index.css for consistent application appearance.
    - Set up the main application entry point in index.js.
    - Included logo.svg for branding purposes.
    - Added performance reporting with reportWebVitals.js.
    - Configured testing environment with setupTests.js.

---
+++ cafeteria_back-main/app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5222;
+
+app.use(cors());
+app.use(express.json());
+
+const url = process.env.ATLAS_URI;
+global.URL = url;
+
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("MongoDB connection successfully");
+});
+
+const admin = require('./routes/admin.js');
+app.use('/admin', admin);
+
+const user = require('./routes/user.js');
+app.use('/user', user);
+
+const promotion = require('./routes/promotion.js');
+app.use('/promotion', promotion);
+
+const support = require('./routes/support.js');
+app.use('/support', support);
+
+const feedback = require('./routes/feedback.js');
+app.use('/feedback', feedback);
+
+const menuItem = require('./routes/menuItem.js');
+app.use('/menuItem', menuItem);
+
+const inventoryItem = require('./routes/inventoryItem.js');
+app.use('/inventoryItem', inventoryItem);
+
+const salary = require('./routes/salary.js');
+app.use('/salary', salary);
+
+const payOrder = require('./routes/payOrder.js');
+app.use('/payOrder', payOrder);
+
+const codOrder = require('./routes/codOrder.js');
+app.use('/codOrder', codOrder);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
